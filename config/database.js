e3329d3a94e0ed5ba6152076b15300a0e6fa7aeb
Dennis <dnnsmoyo@gmail.com>
2017-06-05 09:25:26 +0200

    Add files via upload

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'root',
+        'password': 'Forever'
+    },
+	'database': 'people',
+    'users_table': 'users'
+};
